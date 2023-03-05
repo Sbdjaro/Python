dc60f464f02375cc038740156b052956757f5a4d
Kirill <kir.tearo@yandex.ru>
2023-03-05 14:47:09 +0300

    Complite all flags

--- Task_1/cow_say.py
+++ Task_1/cow_say.py
@@ -1,12 +1,13 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Process some integers.')
+parser = argparse.ArgumentParser()
 parser.add_argument("-e",'--eye_string', type=str, help="Change eye string")
 parser.add_argument("-f",'--cowfile', type=str, help="Change cow")
-#parser.add_argument("-h",'--help')
 parser.add_argument("-l",action="store_true")
-parser.add_argument("-n",action="store_true")                    
+parser.add_argument("-n",action="store_true")    
+parser.add_argument("-T",'--tongue_string', type=str, help="Change cow")      
+parser.add_argument("-W", '--column', type=int,help='Change width')          
 parser.add_argument('message')
 args = parser.parse_args()
 params={}
@@ -14,18 +15,14 @@ if args.eye_string is not None:
 	params['eyes'] = args.eye_string
 if args.cowfile is not None:
 	params['cow'] = args.cowfile
+if args.tongue_string is not None:
+	params['tongue'] = args.tongue_string
+if args.column is not None:
+	params['width'] = args.column
 params['message']=args.message
 from cowsay import cowsay
 from cowsay import list_cows
-if args.l is None:
+if not args.l :
 	print(cowsay(**params))
 else:
 	print(str(list_cows()))
-message = """
-The most remarkable thing about my mother is that for thirty years she served
-the family nothing but leftovers.  The original meal has never been found.
-		--
-""".strip()
-#print(cowsay(message))
-
-#subprocess.run(['cowsay',message])
