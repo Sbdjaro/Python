27057d5fe1f3dd60c14486955e4fad0c3c347c9d
Kirill <kir.tearo@yandex.ru>
2023-03-05 09:34:41 +0300

    Added main functionality in cow_say

--- Task_1/cow_say.py
+++ Task_1/cow_say.py
@@ -1,15 +1,31 @@
-import cowsay
+
 import argparse
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument("-l")
-
-parser.parse_args()
+parser.add_argument("-e",'--eye_string', type=str, help="Change eye string")
+parser.add_argument("-f",'--cowfile', type=str, help="Change cow")
+#parser.add_argument("-h",'--help')
+parser.add_argument("-l",action="store_true")
+parser.add_argument("-n",action="store_true")                    
+parser.add_argument('message')
+args = parser.parse_args()
+params={}
+if args.eye_string is not None:
+	params['eyes'] = args.eye_string
+if args.cowfile is not None:
+	params['cow'] = args.cowfile
+params['message']=args.message
 from cowsay import cowsay
-
+from cowsay import list_cows
+if args.lis None:
+	print(cowsay(**params))
+else:
+	print(list_cows())
 message = """
 The most remarkable thing about my mother is that for thirty years she served
 the family nothing but leftovers.  The original meal has never been found.
-		-- Calvin Trillin
+		--
 """.strip()
-print(cowsay(message))
+#print(cowsay(message))
+
+#subprocess.run(['cowsay',message])
